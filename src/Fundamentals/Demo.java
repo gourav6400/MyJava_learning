18b1866aedbeeddd7ccd36b71b0f2141a3cf46c5
goura <goura@Krishna>
2023-03-23 17:55:21 +0530

    Second

---
+++ src/Fundamentals/Demo.java
@@ -0,0 +1,10 @@
+package Fundamentals;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("jay shree krishna ");
+	}
+
+}
